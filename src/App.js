5eb140d7f7adfb7e5811e57a2b9a7a81ace1556d
Joey Baker <joey@discordapp.com>
2019-10-03 09:32:31 -0700

    Add UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styles from "./App.module.css";
+import { get } from "axios";
+import throttle from "lodash.throttle";
+import LRU from "quick-lru";
 
-function App() {
+const queryCache = new LRU({ maxSize: 100 });
+
+export default function App() {
+  const [data, setData] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState("");
+  const fetchQuery = React.useCallback(
+    throttle(async e => {
+      const { value } = e.target;
+      if (!value) {
+        setData([]);
+        return;
+      }
+      if (queryCache.has(value)) {
+        setData(queryCache.get(value));
+        return;
+      }
+      setLoading(true);
+      try {
+        const { data } = await get("/api?q=" + value);
+        setData(data.predictions);
+        queryCache.set(value, data.predictions);
+      } catch (e) {
+        setError(e.toString());
+      }
+      setLoading(false);
+    }, 50),
+    [setData]
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <input type="search" onInput={fetchQuery} />
+      {loading && <mark>…</mark>}
+      {error ? (
+        <div>{error}</div>
+      ) : (
+        <ul className={styles.list}>
+          {data.map(({ id, structured_formatting: { main_text: text } }) => (
+            <li key={id}>{text}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
-
-export default App;
